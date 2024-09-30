2f2a265e6cd78b3b38105f51463971d9885645d8
dsv <zxcv>
2024-09-30 23:34:39 +0500

    Initial Commit

---
+++ Video donloader Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Video donloader Project
+//
+//  Created by Muhammad Rizwan on 30/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Video donloader Project/Video_donloader_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_donloader_ProjectApp.swift
+//  Video donloader Project
+//
+//  Created by Muhammad Rizwan on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Video_donloader_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
